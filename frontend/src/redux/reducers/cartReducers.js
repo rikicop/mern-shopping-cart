44298fd1364d4ba513556b06a87f60ac2531638d
rikicop <ruperto1@protonmail.com>
2021-12-28 00:20:27 -0400

    comentarios explicacion 1

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -9,8 +9,9 @@ export const cartReducer = (state = CART_INITIAL_STATE, action) => {
     case actionTypes.ADD_TO_CART:
       const item = action.payload;
 
+      //payload: {product: data._id} <-- action.payload
       const existItem = state.cartItems.find((x) => x.product === item.product);
-
+      console.log("state.cartItems: ", state.cartItems);
       if (existItem) {
         return {
           ...state,
